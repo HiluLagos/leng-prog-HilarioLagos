d0a3461b71765fea94f0686f8f937c1b4e0796e7
HiluLagos <128090306+HiluLagos@users.noreply.github.com>
2024-08-05 17:57:53 -0300

    Trabajo terminado

--- haskell-exercises/collatz-conjecture/src/CollatzConjecture.hs
+++ haskell-exercises/collatz-conjecture/src/CollatzConjecture.hs
@@ -1,4 +1,9 @@
 module CollatzConjecture (collatz) where
 
 collatz :: Integer -> Maybe Integer
-collatz = error "You need to implement this function."
+collatz n
+  | n < 1 = Nothing
+  | n == 1 = Just 0
+  | otherwise = if n `mod` 2 == 0
+                then fmap (+1) (collatz (n `div` 2))
+                else fmap (+1) (collatz (3 * n + 1))
