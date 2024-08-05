dbcc8262a28c64ae1a1c12dc5153072172ec415c
HiluLagos <128090306+HiluLagos@users.noreply.github.com>
2024-08-05 17:55:31 -0300

    Trabajo completo

--- haskell-exercises/fraction/src/Fraction.hs
+++ haskell-exercises/fraction/src/Fraction.hs
@@ -1,31 +1,29 @@
-module Fraction (Fraction, add, sub, mul, divide, hcf) where
+module Fraction (Fraction, add, sub, mul, divide, hcf, simpl) where
 
 type Fraction = (Int, Int)
 
--- Implement the `add` Function
+-- simplify method
+simpl :: Fraction -> Fraction
+simpl (n, d) = if hf > 1 then (n `div` hf, d `div` hf) else (n, d)
+  where
+    hf = hcf n d
 
+-- Implement the `add` Function
 add :: Fraction -> Fraction -> Fraction
-add n d = error "Implement it"
+add (n1, d1) (n2, d2) = if d1 == d2 then simpl (n1+n2, d1) else simpl (n1*d2 + n2*d1, d1*d2)
 
 -- Implement the `sub` Function
-
 sub :: Fraction -> Fraction -> Fraction
-sub n d = error "Implement it"
+sub (n1, d1) (n2, d2) = if d1 == d2 then simpl (n1-n2, d1) else simpl (n1*d2 - n2*d1, d1 * d2)
 
 -- Implement the `mul` Function
-
 mul :: Fraction -> Fraction -> Fraction
-mul n d = error "Implement it"
+mul (n1, d1) (n2, d2) = simpl (n1 * n2, d1 * d2)
 
 -- Implement the `divide` Function
-
 divide :: Fraction -> Fraction -> Fraction
-divide n d = error "Implement it"
+divide (n1, d1) (n2, d2) = simpl (n1 * d2, d1 * n2)
 
 -- Implement the `hcf` Function
-
 hcf :: Int -> Int -> Int
-hcf n d = error "Implement it"
-
-
-    
+hcf n d = gcd n d
