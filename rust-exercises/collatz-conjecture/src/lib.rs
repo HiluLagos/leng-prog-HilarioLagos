8ebafd68782f3191304a27117595b94976f38211
HiluLagos <128090306+HiluLagos@users.noreply.github.com>
2024-09-30 17:47:38 -0300

    Collatz completed

--- rust-exercises/collatz-conjecture/src/lib.rs
+++ rust-exercises/collatz-conjecture/src/lib.rs
@@ -1,4 +1,21 @@
 pub fn collatz(mut n: u64) -> Option<u64> {
-  None
+  collatz_loop(n, 0)
 }
 
+
+pub fn collatz_loop(mut n: u64, s: u64) -> Option<u64> {
+  if n <= 0{
+    None
+  }
+  else{
+    if n == 1{
+      Some(s)
+    }
+    else if n%2 == 0{
+      collatz_loop(n/2, s+1)
+    }
+    else{
+      collatz_loop(3*n+1, s+1)
+    }
+  }
+}
