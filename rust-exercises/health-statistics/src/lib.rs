f408e952aae187053fd5b3a92cf0f67e36f151dd
HiluLagos <128090306+HiluLagos@users.noreply.github.com>
2024-10-14 13:58:03 -0300

    Helth statistics completed

--- rust-exercises/health-statistics/src/lib.rs
+++ rust-exercises/health-statistics/src/lib.rs
@@ -1,3 +1,5 @@
+use std::ffi::CString;
+
 pub struct User {
     name: String,
     age: u32,
@@ -5,15 +7,15 @@ pub struct User {
 }
 
 impl User {
-    pub fn new(name: &str, age: u32, weight: f32) -> Self { panic!("Implement me!") }
+    pub fn new(name: &str, age: u32, weight: f32) -> Self { User {name : name.parse::<String>().unwrap(), age, weight} }
 
-    pub fn name(&self) -> &str { panic!("Implement me!") }
+    pub fn name(&self) -> &str { &self.name }
 
-    pub fn age(&self) -> u32 { panic!("Implement me!") }
+    pub fn age(&self) -> u32 { self.age }
 
-    pub fn weight(&self) -> f32 { panic!("Implement me!") }
+    pub fn weight(&self) -> f32 { *(&self.weight) }
 
-    pub fn set_age(&mut self, new_age: u32) { panic!("Implement me!") }
+    pub fn set_age(&mut self, new_age: u32) { self.age = new_age }
 
-    pub fn set_weight(&mut self, new_weight: f32) { panic!("Implement me!") }
+    pub fn set_weight(&mut self, new_weight: f32) { self.weight = new_weight }
 }
