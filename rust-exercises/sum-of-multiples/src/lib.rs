699a910876c4a185c85298728cfaf5b6725ad143
HiluLagos <128090306+HiluLagos@users.noreply.github.com>
2024-11-03 21:25:42 -0300

    Sum of Multiples complete

--- rust-exercises/sum-of-multiples/src/lib.rs
+++ rust-exercises/sum-of-multiples/src/lib.rs
@@ -1,3 +1,8 @@
+fn is_div(n: u32, factors: &[u32]) -> bool {
+    factors.iter().any(|&f| n%f == 0)
+}
+
+
 pub fn sum_of_multiples(limit: u32, factors: &[u32]) -> u32 {
-    0
+    (1..limit).filter(|&n| is_div(n, factors)).sum()
 }
