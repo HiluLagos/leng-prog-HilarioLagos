b6052151372342c23408e01967f065c2edf4fb4c
HiluLagos <128090306+HiluLagos@users.noreply.github.com>
2024-11-04 09:55:54 -0300

    Word count completed

--- rust-exercises/word-count/src/lib.rs
+++ rust-exercises/word-count/src/lib.rs
@@ -2,5 +2,15 @@ use std::collections::HashMap;
 
 /// Count occurrences of words.
 pub fn word_count(phrase: &str) -> HashMap<String, u32> {
-  panic!("Implement me!")
+  let words: Vec<&str> = phrase.split_whitespace().collect();
+  let mut ocurrance_map: HashMap<String, u32> = HashMap::new();
+  for word in words {
+    let lower_word = word.to_lowercase();
+    if let Some(&count) = ocurrance_map.get(&lower_word) {
+      ocurrance_map.insert(lower_word, count + 1);
+    } else {
+      ocurrance_map.insert(lower_word, 1);
+    }
+  }
+  ocurrance_map
 }
